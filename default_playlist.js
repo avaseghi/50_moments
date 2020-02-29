461c757566b1dd210fabe9c442770122aea91160
avaseghi <af3412@nyu.edu>
2020-02-28 19:37:49 -0500

    added default_playlist.js

---
+++ default_playlist.js
@@ -0,0 +1,202 @@
+module.exports = [
+  {
+    "title": "Amandah",
+    "index": 0
+  },
+  {
+    "title": "Amy",
+    "index": 1
+  },
+  {
+    "title": "Bernice",
+    "index": 2
+  },
+  {
+    "title": "Bob",
+    "index": 3
+  },
+  {
+    "title": "Carlos",
+    "index": 4
+  },
+  {
+    "title": "Charise",
+    "index": 5
+  },
+  {
+    "title": "Chingy",
+    "index": 6
+  },
+  {
+    "title": "Chris",
+    "index": 7
+  },
+  {
+    "title": "Danny",
+    "index": 8
+  },
+  {
+    "title": "Debra",
+    "index": 9
+  },
+  {
+    "title": "Denise",
+    "index": 10
+  },
+  {
+    "title": "Ernest",
+    "index": 11
+  },
+  {
+    "title": "Gage",
+    "index": 12
+  },
+  {
+    "title": "Guy",
+    "index": 13
+  },
+  {
+    "title": "Hassan",
+    "index": 14
+  },
+  {
+    "title": "Indie",
+    "index": 15
+  },
+  {
+    "title": "James",
+    "index": 16
+  },
+  {
+    "title": "Jana",
+    "index": 17
+  },
+  {
+    "title": "Jay",
+    "index": 18
+  },
+  {
+    "title": "Jim",
+    "index": 19
+  },
+  {
+    "title": "Joanne",
+    "index": 20
+  },
+  {
+    "title": "John",
+    "index": 21
+  },
+  {
+    "title": "Jose",
+    "index": 22
+  },
+  {
+    "title": "Jrae",
+    "index": 23
+  },
+  {
+    "title": "Kaweeta",
+    "index": 24
+  },
+  {
+    "title": "Kevin",
+    "index": 25
+  },
+  {
+    "title": "Lara",
+    "index": 26
+  },
+  {
+    "title": "Melissa",
+    "index": 27
+  },
+  {
+    "title": "Michael",
+    "index": 28
+  },
+  {
+    "title": "Michael",
+    "index": 29
+  },
+  {
+    "title": "Milo",
+    "index": 30
+  },
+  {
+    "title": "Narayan",
+    "index": 31
+  },
+  {
+    "title": "Nicole",
+    "index": 32
+  },
+  {
+    "title": "Oleg",
+    "index": 33
+  },
+  {
+    "title": "Paula",
+    "index": 34
+  },
+  {
+    "title": "Paula",
+    "index": 35
+  },
+  {
+    "title": "Ramy",
+    "index": 36
+  },
+  {
+    "title": "Raphael",
+    "index": 37
+  },
+  {
+    "title": "Rene",
+    "index": 38
+  },
+  {
+    "title": "Richard",
+    "index": 39
+  },
+  {
+    "title": "Rico",
+    "index": 40
+  },
+  {
+    "title": "Sam",
+    "index": 41
+  },
+  {
+    "title": "Scott",
+    "index": 42
+  },
+  {
+    "title": "Shawn",
+    "index": 43
+  },
+  {
+    "title": "Skylaranne",
+    "index": 44
+  },
+  {
+    "title": "Teresa",
+    "index": 45
+  },
+  {
+    "title": "Turtle",
+    "index": 46
+  },
+  {
+    "title": "Val",
+    "index": 47
+  },
+  {
+    "title": "Victor",
+    "index": 48
+  },
+  {
+    "title": "Victoria",
+    "index": 49
+  }
+]
