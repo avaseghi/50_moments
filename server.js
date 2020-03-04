306a5911cad5b63beebf14d1d99313b98c87e526
avaseghi <af3412@nyu.edu>
2020-03-04 10:32:13 -0500

    programmed queue tospit out number

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express = require('express'),
     app = express(),
     currentVid,
     vidQueue = [],
-    default_playlist = require('./default_playlist'),
-    default_index = 0;
+    videos = ["AMANDAH", "AMY", "BERNICE", "BOB", "CARLOS", "CHARISE", "CHINGY", "CHRIS", "DANNY", "DEBRA", "DENISE", "ERNEST", "GAGE", "GUY", "HASSAN", "INDIE", "JAMES", "JANA", "JAY", "JIM", "JOANNE", "JOHN", "JOSE", "JRAE", "KAWEETA", "KEVIN", "LARA", "MELISSA", "MICHAEL", "MICHAEL", "MILO", "NARAYAN", "NICOLE", "OLEG", "PAULA", "PAULA", "RAMY", "RAPHAEL", "RENE", "RICHARD", "RICO", "SAM", "SCOTT", "SHAWN", "SKYLARANNE","TERESA", "TURTLE", "VAL", "VICTOR", "VICTORIA"],
+    dIndex = 0;
 
 app.set('view engine', 'ejs');
 
@@ -16,60 +16,55 @@ app.get('/queue', function(req, res) {
     currentVid = vidQueue[0];
     vidQueue.shift();
   } else {
-    currentVid = default_playlist[default_index];
-    if (default_index < default_playlist.length - 1) {
-      default_index ++;
+    currentVid = dIndex;
+    if (dIndex < videos.length - 1) {
+      dIndex += 1;
     } else {
-      default_index = 0;
+      dIndex = 0;
     }
   }
-  res.send(currentVid);
+  res.send(200, currentVid);
 });
 
 app.get('/add', function(req, res) {
   if (req.query.video_number) {
-    var data = {
-      "title": default_playlist[req.query.video_number],
-      "index": req.query.video_number
-    }
-    console.log(vidQueue);
-    addVideoToQueue(data, res);
+    var vidIndex = req.query.video_number;
+    addVideoToQueue(vidIndex, res);
   }
 });
 
 function addVideoToQueue(video_data, res) {
   var vidInQueue = checkQueue(video_data);
 
-  if (currentVid && currentVid.index == video_data.index) {
+  if (currentVid && currentVid == video_data) {
 
-      var message = video_data.title + " is currently playing."
+      var message = videos[video_data] + " is currently playing."
 
   } else if (vidInQueue) {
 
     if (vidInQueue > 1) {
 
-      var message = video_data.title + " is number " + (vidInQueue + 1) + " in the queue."
+      var message = videos[video_data] + " is number " + (vidInQueue + 1) + " in the queue."
 
     } else {
 
-      var message = video_data.title + " is next in the queue."
+      var message = videos[video_data] + " is next in the queue."
     }
 
   } else {
 
     vidQueue.push(video_data);
 
-    var message = "You have added " + video_data.title + " to the queue."
+    var message = "You have added " + videos[video_data] + " to the queue."
     console.log(message);
   }
 
   renderVerificationMessage(message, res);
 }
 
-function checkQueue(video) {
-  console.log("vide index " + video.index);
+function checkQueue(videoIndex) {
   for(i = 0; i < vidQueue.length; i ++) {
-    if (vidQueue[i].index == video.index) {
+    if (vidQueue[i] == videoIndex) {
       return i + 1
     }
   }
@@ -80,6 +75,6 @@ function renderVerificationMessage(message, res) {
   res.render('template.ejs', {verification: message});
 }
 
-app.listen(80, function () {
+app.listen(8000, function () {
   console.log('Server running!')
 })
